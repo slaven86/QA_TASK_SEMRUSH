4e9812f7a4d28d4d546f87e8b8dc9bd11ac7e6e8
slaven86 <slaven.lakota@gmail.com>
2025-10-06 09:37:07 +0200

    small change

--- tests/search_prowly_ai_tools.spec.ts
+++ tests/search_prowly_ai_tools.spec.ts
@@ -6,23 +6,10 @@ test('Search for "prowly ai tools" images by Type', async ({ page }) => {
   await page.getByPlaceholder('Search the web...').press('Enter')
   await page.getByRole('menuitem', { name: 'Images' }).click()
   await expect(page.getByRole('menuitem', { name: 'Images' })).toHaveClass(/tab--highlighted/)
-  await page.getByRole('menuitem', { name: ' All types ' }).click()
+  await page.getByRole('menuitem', { name: ' All tyres ' }).click()
   await page.getByRole('menuitem', { name: ' GIF ' }).click()
   const selectedType = page.locator('div[data-test-id="search-filter-imageType"].filter--selected .filter__text')
   await expect(selectedType).toHaveText('GIF')
 
 })
 
-
-test('Test slaven', async ({ page }) => {
-  await page.goto('https://www.ecosia.org/')
-  await page.getByPlaceholder('Search the web...').fill('prowly ai tools')
-  await page.getByPlaceholder('Search the web...').press('Enter')
-  await page.getByRole('menuitem', { name: 'Images' }).click()
-  await expect(page.getByRole('menuitem', { name: 'Images' })).toHaveClass(/tab--highlighted/)
-  await page.getByRole('menuitem', { name: ' All types ' }).click()
-  await page.getByRole('menuitem', { name: ' GIF ' }).click()
-  const selectedType = page.locator('div[data-test-id="search-filter-imageType"].filter--selected .filter__text')
-  await expect(selectedType).toHaveText('GIF')
-
-})
