315343ec612cc3e98415e80f29c9e8276e807658
slaven86 <slaven.lakota@gmail.com>
2025-10-06 10:13:30 +0200

    test fix

--- tests/search_prowly_ai_tools.spec.ts
+++ tests/search_prowly_ai_tools.spec.ts
@@ -6,7 +6,7 @@ test('Search for "prowly ai tools" images by Type', async ({ page }) => {
   await page.getByPlaceholder('Search the web...').press('Enter')
   await page.getByRole('menuitem', { name: 'Images' }).click()
   await expect(page.getByRole('menuitem', { name: 'Images' })).toHaveClass(/tab--highlighted/)
-  await page.getByRole('menuitem', { name: ' All tyres ' }).click()
+  await page.getByRole('menuitem', { name: ' All types ' }).click()
   await page.getByRole('menuitem', { name: ' GIF ' }).click()
   const selectedType = page.locator('div[data-test-id="search-filter-imageType"].filter--selected .filter__text')
   await expect(selectedType).toHaveText('GIF')
